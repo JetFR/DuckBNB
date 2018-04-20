ad4e97109a0966c0c5fed3768943e71bd9a8f36f
JET-PC <jeanetienne.trapon@gmail.com>
2018-04-20 16:01:33 +0200

    MODIF lien tabs et noms

---
+++ src/pages/mining/mining.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'mining.html'
+})
+export class MiningPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/profile/modif/modif.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+
+@NgModule({
+  declarations: [
+    ModifPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ModifPage),
+  ],
+})
+export class ModifPage {}
