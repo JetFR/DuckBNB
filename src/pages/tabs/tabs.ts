415f19f891c6b4a6922d10ebbf6363a7bfc231f5
Quentin Strauss <q.strauss@eutech-ssii.com>
2018-04-20 16:34:45 +0200

    integ()

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import { MiningPage } from '../mining/mining';
-import { WalletPage } from '../wallet/wallet';
-import { ProfilePage } from '../profile/profile';
-import { SettingsPage } from '../settings/settings';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,11 +10,10 @@ import { SettingsPage } from '../settings/settings';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = MiningPage;
-  tab3Root = WalletPage;
-  tab4Root = ProfilePage;
-  tab5Root = SettingsPage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
+
   constructor() {
-  
+
   }
 }
